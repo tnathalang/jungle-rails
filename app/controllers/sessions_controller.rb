bc0505cbe21438bb507174bd2eda5fe09a3cccba
vagrant <nathalang_t@hotmail.ca>
2019-02-21 18:07:00 +0000

    add login message on application layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
         user = User.find_by_email(params[:email])
         #if user exists and the password is correct.
         if user && user.authenticate(params[:password])
+            #save uder id inside browser cookie.
             session[:user_id] = user.user_id
             redirect_to '/'
         else
@@ -14,6 +15,8 @@ class SessionsController < ApplicationController
     end
 
     def destroy
+        session[:user_id] = nil 
+        redirect_to '/login'
     end
 
 end
