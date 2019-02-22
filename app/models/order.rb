8bbc2fcdaf5ecf1122eee2bd929fa57dd48276e3
vagrant <nathalang_t@hotmail.ca>
2019-02-22 18:18:53 +0000

    cart empty if statement

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,6 @@ class Order < ActiveRecord::Base
 
   monetize :total_cents, numericality: true
 
-  validates :stripe_charge_id, presence: true
-  validates :quantity, numericality: {greater_than: 0}
+  validates :stripe_charge_id, presence: true 
+
 end
