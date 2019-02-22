d8a8cd5a6bbce7da1aff445aeeeb44e7c897939d
vagrant <nathalang_t@hotmail.ca>
2019-02-22 20:32:30 +0000

    modify review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
     belongs_to :product
+
+    validates :product.name, presence: true
+    validates :current_user, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true
+
+
 end
