4a0b548cb22545b6854fe9430d7a2d2ab2442e13
vagrant <nathalang_t@hotmail.ca>
2019-02-23 19:47:03 +0000

    Review controller create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,26 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    # raise "Yay, I'm here!"
+    product = Product.find(paramns[:product_id])
+    review = Review.new(review_params)
+    review.product = product
+
+    product.review.new(review_params)
+    
+    if review.save
+      #/products/:id/
+      redirect_to product_path(params[:product_id])
+    end
   end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+
+
 end
