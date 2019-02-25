b356c4f86e6d0e8827ce6b6c6521dcaf244e8a95
vagrant <nathalang_t@hotmail.ca>
2019-02-25 20:51:37 +0000

    add rspec tests to product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,21 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
  describe 'Validation' do
-  it 'Should have a name' do
-  @product = Product.new
-  expect(@product.name).to be_present
-  end
-  it 'Should have a price' do
-    @product = Product.new
-    expect(@product.price).to be_present
-  end
-  it 'Should have a quantity' do
-    @product = Product.new
-    expect(@product.quantity).to be_present
-   end
-  it 'Should have a category' do
-    @product = Product.new
-    expect(@product.category).to be_present
+    before(:each) do
+      @product = Product.new
+      @product.name = "Chair"
+      @product.price = 70
+      @product.quantity = 10
+      @product.category = Category.new(name: "Furniture")
+    end
+    it 'Should have a name' do
+      @product.name = nil 
+      expect(@product).to_not be_valid
+    end
+    it 'Should have a price' do
+      @product.price_cents = nil 
+      expect(@product).to_not be_valid
+    end
+    it 'Should have a quantity' do
+      @product.quantity = nil 
+      expect(@product).to_not be_valid
+    end
+    it 'Should have a category' do
+      @product.category = nil 
+      expect(@product).to_not be_valid
+    end
+    it 'should have error messages on nil' do
+      @product.name = nil 
+      @product.valid?
+      expect(@product.errors.full_messages).to include('Name can\'t be blank')
     end
   end
 end
