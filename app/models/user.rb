2d49b67b235586ddddaca9052458aa7093eff550
vagrant <nathalang_t@hotmail.ca>
2019-02-26 19:18:19 +0000

    re-add User model/accidental delete

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+    has_secure_password
+    has_many :reviews
+
+    before_validation :strip_whitespace
+    before_save :downcase_email
+    
+    validates :name, presence: true 
+    validates :email, presence: true, uniqueness: {case_sensitive: true}
+    validates :password, presence: true, confirmation: true, length: {minimum: 6}
+    validates_presence_of :password
+
+    def self.authenticate_with_credentials(email,password)
+        # look for user with email inside the db
+        # @user = User.find_by_email(email)
+        User.find_by_email(email).try(:authenticate, password)
+    
+    end 
+
+    def strip_whitespace
+        if self.email
+            self.email = self.email.strip
+        end
+       
+        if self.name 
+            self.name = self.name.strip
+        end
+    end
+
+    def downcase_email
+        self.email.downcase!
+    end
+end
